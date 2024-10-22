01fafe59efe676ab1d642773c92d1f8cfc4b3bee
[Gaye Dinç] <gayedinc190@gmail.com>
2024-10-22 10:53:34 +0300

    rock-paper-scissors güncelleme yapıldı

--- assets/js/tas-kagit-makas.js
+++ assets/js/tas-kagit-makas.js
@@ -1,29 +1,77 @@
-let userChoice = prompt("taş, kağıt makas?");
+const choiceList = ["Taş", "Kağıt", "Makas"];
+let randomNumber = 0;
 
-let computerChoice = ["taş", "kağıt", "makas"].at(Math.floor(Math.random() * 3));
+const getComputerChoice = () => {
+  randomNumber = Math.floor(Math.random() * 3);
+  return (choiceList[randomNumber]);
+};
 
+rock.addEventListener('click', handleRockBtn);
+paper.addEventListener('click', handlePaperBtn);
+scissors.addEventListener('click', handleScissorsBtn);
 
-if (userChoice === computerChoice) {
-  console.log('Berabere');
+function handleRockBtn() {
+  const computerChoiceValue = getComputerChoice();
+  computerChoice.innerHTML = `<button>${computerChoiceValue}</button>`;
+  winnerBtnTxt.innerHTML = "";
+  if (computerChoiceValue === "Kağıt") {
+    computerScore++;
+    computerScoreTxt.innerText = computerScore;
+    winnerBtnTxt.innerHTML += 'Bilgisayar';
+  } else if (computerChoiceValue === "Makas") {
+    userScore++;
+    userScoreTxt.innerText = userScore;
+    winnerBtnTxt.innerHTML += 'Oyuncu';
+  } else if (computerChoiceValue === "Taş") {
+    winnerBtnTxt.innerHTML = "Yok";
+  }
 
-} else if (userChoice === 'taş' && computerChoice === 'kağıt') {
-  console.log('Bilgisayar kazandı, kağıt taşı sarar :(' + " " + 'Bilgisayarın seçimi: ' + computerChoice);
-
-} else if (userChoice === 'taş' && computerChoice === 'makas') {
-  console.log('Sen kazandın, taş makası kırar!' + " " + 'Bilgisayarın seçimi: ' + computerChoice);
-
-} else if (userChoice === 'kağıt' && computerChoice === 'taş') {
-  console.log('Sen kazandın, kağıt taşı sarar!' + " " + 'Bilgisayarın seçimi: ' + computerChoice);
-
-} else if (userChoice === 'kağıt' && computerChoice === 'makas') {
-  console.log('Bilgisayar kazandı, makas kağıdı keser :(' + " " + 'Bilgisayarın seçimi: ' + computerChoice);
 }
-else if (userChoice === 'makas' && computerChoice === 'taş') {
-  console.log('Bilgisayar kazandı, taş makası kırar :(' + " " + 'Bilgisayarın seçimi: ' + computerChoice);
+
+function handlePaperBtn() {
+  const computerChoiceValue = getComputerChoice();
+  computerChoice.innerHTML = `<button>${computerChoiceValue}</button>`;
+  winnerBtnTxt.innerHTML = "";
+  if (computerChoiceValue === "Makas") {
+    computerScore++;
+    computerScoreTxt.innerText = computerScore;
+    winnerBtnTxt.innerHTML += 'Bilgisayar';
+  } else if (computerChoiceValue === "Taş") {
+    userScore++;
+    userScoreTxt.innerText = userScore;
+    winnerBtnTxt.innerHTML += 'Oyuncu';
+  } else if (computerChoiceValue === "Kağıt") {
+    winnerBtnTxt.innerHTML = "Yok";
+  }
 }
-else if (userChoice === 'makas' && computerChoice === 'kağıt') {
-  console.log('Sen kazandın, makas kağıdı keser!' + " " + 'Bilgisayarın seçimi: ' + computerChoice);
+
+function handleScissorsBtn() {
+  const computerChoiceValue = getComputerChoice();
+  computerChoice.innerHTML = `<button>${computerChoiceValue}</button>`;
+  winnerBtnTxt.innerHTML = "";
+
+  if (computerChoiceValue === "Taş") {
+    computerScore++;
+    computerScoreTxt.innerText = computerScore;
+    winnerBtnTxt.innerHTML += 'Bilgisayar';
+  } else if (computerChoiceValue === "Kağıt") {
+    userScore++;
+    userScoreTxt.innerText = userScore;
+    winnerBtnTxt.innerHTML += 'Oyuncu';
+  } else if (computerChoiceValue === "Makas") {
+    winnerBtnTxt.innerHTML = "Yok";
+  }
 }
-else {
-  console.log('Lütfen taş, kağıt veya makas giriniz.');
+
+let userScore = 0;
+let computerScore = 0;
+
+clearBtn.addEventListener("click", handleClearBtn);
+function handleClearBtn() {
+  userScoreTxt.innerText = 0;
+  computerScoreTxt.innerText = 0;
+  winnerBtnTxt.innerHTML = "";
+  computerChoice.innerHTML = "";
+  userScore = 0;
+  computerScore = 0;
 }
